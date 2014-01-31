708a765ae5106edad000d058bb4a060d96674e15
Vipul Harsh <vipulharsh.iit@gmail.com>
2014-01-31 18:43:16 +0530

    redirections working properly

--- lab4/lab4/pipedExec.c
+++ lab4/lab4/pipedExec.c
@@ -4,6 +4,24 @@
 #include <sys/wait.h>
 
 
+
+
+
+opIndex(char *ch){
+	
+	if(strcmp(ch , "<") == 0) return 0;
+	if(strcmp(ch , ">") == 0) return 1;
+	if(strcmp(ch , ">>") == 0) return 1;
+	
+}
+
+
+
+
+
+
+
+
 void pipedExec(command commands){
   
     int noOfCommands = commands.nTokens;
@@ -12,41 +30,52 @@ void pipedExec(command commands){
     int outPipe;
     int prevOutpipe;
     int j=0; 
-    int *pipefd =malloc(sizeof(int)*2);     
+    int pipefd[2];     
     if(commands.nTokens ==1){
         execute(tokenize(commands.tokens[0]));
         return; 
     }
-    for(i=0,j=0;i<noOfCommands-1;i++,j++){
-
+    for(i=0,j=0;i<noOfCommands-1;i=i+2,j++){
+/*
         if(strcmp(commands.tokens[j],"|") !=0){
            printf("Error in command \n");
             return; 
+		}
+*/		
 
-        }
+		printf(" now %s \n", commands.tokens[i]);
+
         if (pipe(pipefd)) {
             perror("pipe");
 	        exit(127);
         
         }
+        
         inPipe = pipefd[0];
         outPipe = pipefd[1];
         
         child_process_ID = fork();
+        
         if(child_process_ID ==-1){
                 printf("fork error");
                 exit(1);
                 break;
         }
         else if(child_process_ID ==0){
-                if(i ==0){
+                if(i==0){
+					biggestParent = 0;
                     close(inPipe);
+                    printf(" 2comes here ::  \n");
                     dup2(outPipe,1);
+                  //  printf(" 3comes here %d ::  \n");
+                    close(outPipe);
+                    //printf("comes here ::  \n");
                     execute3(tokenize(commands.tokens[i]));
-                    break;
+					kill(0,9);
                 }
                 else{
-                    close(inPipe);
+          //          close(inPipe);
+                 //   sleep(1);
                     dup2(prevOutpipe,0);
                     dup2(outPipe,1); 
                     execute3(tokenize(commands.tokens[i]));
@@ -54,43 +83,69 @@ void pipedExec(command commands){
                 }
         } 
         else{
+                int s;
+     //           wait(&s);
+    //            kill(
                 prevOutpipe = inPipe;
                 continue;
         }
         
-    }
+   }
+ 
+if(biggestParent == 1){    
+ //  	printf(" now %s \n", commands.tokens[i]);
+ //   sleep(1);
     dup2(prevOutpipe,0);
-    close(inPipe);
+ // printf("parent now %s \n", commands.tokens[i]);
+	close(inPipe);
     execute3(tokenize(commands.tokens[i]));
+  }  
 }
 
 
 void IORedirection(command commands){
 	
-	printf("comes here : %d \n  ",commands.nTokens);
+//	printf("No of Tokens: %d \n  ",commands.nTokens);
 	
     int i=0;
     int inPipe ;
     int outPipe;
     int prevOutpipe;
     
-    int *pipefd =malloc(sizeof(int)*2);    
+    
+    int count[3];
+    for(int h=0;h<3;h++)
+		count[h] = 0;
+    
+    
+    int j;
+    for(j=0;j<commands.nTokens;j++){
+			
+			if((strcmp(commands.tokens[j],">")==0) ||(strcmp(commands.tokens[j],"<")==0) || (strcmp(commands.tokens[j],">>")==0)){
+				count[opIndex(commands.tokens[j])]++;
+				if(count[opIndex(commands.tokens[j])] > 1){
+						printf("Error : more than 1 \"%s\" Redirections \n" , commands.tokens[j]);
+						return;
+				}	
+			}
+	}		
+    
+    
+    
+    
+    
+    int pipefd[2];   
 
     child_process_ID = fork();
     if(child_process_ID==0){
-        int j=0;
         for(j=0;j<commands.nTokens;j++){
-            if((strcmp(commands.tokens[j],">")==0)){
+			
+	       if((strcmp(commands.tokens[j],">")==0)){
 
                 j++;
                 if(j<commands.nTokens){
-                    
-                    //int newfd = open("a.txt",'w');
-                    
                     commands.tokens[j][strlen(commands.tokens[j]) - 1] = '\0';
-                    
                     int newfd  = open(commands.tokens[j], O_CREAT|O_TRUNC|O_WRONLY, 0644);
-                    printf("here ** %s **" , commands.tokens[j]);
                     dup2(newfd,1);
                 }
             }
@@ -105,9 +160,24 @@ void IORedirection(command commands){
                     dup2(newfd,0);
                 }
             }
+            
+            
+          if((strcmp(commands.tokens[j],">>")==0)){
+
+                j++;
+                if(j<commands.nTokens){
+                   commands.tokens[j][strlen(commands.tokens[j]) - 1] = '\0';
+                   int newfd  = open(commands.tokens[j], O_CREAT|O_APPEND|O_WRONLY , 0644);
+                    //int newfd = open(commands.tokens[j],'r');
+                    dup2(newfd,1);
+                }
+            }
+            
         }
         execute3(tokenize(commands.tokens[0]));
     }
+    
+    
     else{
        int *stat = malloc(sizeof(int));
        while(1){
