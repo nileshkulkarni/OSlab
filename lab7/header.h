d2e4030262467a8ce9a48468cd6ab1ffae739cae
Vipul Harsh <vipulharsh.iit@gmail.com>
2014-02-28 20:58:11 +0530

    working version

--- lab7/header.h
+++ lab7/header.h
@@ -1,10 +1,11 @@
-#define NUM_THREADS  2
-#define MAX_BUFFER_SIZE 20
-
+#define NUM_THREADS  4
+#define MAX_BUFFER_SIZE 2
 
 //enum message_type{SEND , RECEIVE};
 #define SEND 1
 #define RECEIVE 0
+#define DEBUG 0
+
 
 void strip(char *s){
 	int i = strlen(s)-1;
--- lab7/ipc.c
+++ lab7/ipc.c
@@ -6,10 +6,28 @@
 message msg[NUM_THREADS];
 pthread_mutex_t mutex_var[NUM_THREADS];
 pthread_cond_t sem_var[NUM_THREADS];
+
 message messages[MAX_BUFFER_SIZE];
+int lockedMutex[NUM_THREADS];
+
 int empty_block;
 int start_block;
 int bufferFull;
+int deadlock_detected=0;
+
+
+
+
+void destroy_mutex(){
+	int i;
+	for(i=0; i<NUM_THREADS;i++){
+		pthread_mutex_destroy(&mutex_var[i]);
+		pthread_cond_destroy(&sem_var[i]);
+	}
+}
+
+
+
 
 void* process_code(void *arg){ //reads a file for commands and adds it to messages(global variable)
 
@@ -30,8 +48,12 @@ void* process_code(void *arg){ //reads a file for commands and adds it to messag
 	
 	while (fgets(op, sizeof(op), fp)){
 		
+		if(deadlock_detected==1) break;
+		
 		strip(op);
+	#if(DEBUG)
 		printf("Thread %d %s \n", thread_id ,op);
+	#endif
 		if(strcmp(op , "S") == 0){ 
 			fgets(msg[thread_id].msg, sizeof(msg[thread_id].msg), fp);
 			strip(msg[thread_id].msg);
@@ -46,13 +68,14 @@ void* process_code(void *arg){ //reads a file for commands and adds it to messag
 			msg[thread_id].type = RECEIVE;
 		}
 		else{
-			printf("Invalid file : Aborting Thread %d \n" , thread_id);;
-			exit(0);
+			printf("Invalid file : Aborting Thread %d \n" , thread_id);
+			break;
 		}
 	//	printf("Thread %d is waiting \n", thread_id ,op);
 	    pthread_cond_wait(&sem_var[thread_id], &mutex_var[thread_id]); //mutex is temporarily locked at this point
 	//	printf("Thread %d has waited \n", thread_id ,op);
-		
+	
+	#if(DEBUG)	
 		if(strcmp(op , "R") == 0){
 			printf("Received message(--%s--)  at Thread: %d from Thread: %d \n" , 
 								msg[thread_id].msg , thread_id , msg[thread_id].sender);
@@ -60,12 +83,11 @@ void* process_code(void *arg){ //reads a file for commands and adds it to messag
 		else if(strcmp(op , "S") == 0){
 			printf("I am thread %d and msg has been sent \n",thread_id);
 		}
-	
+	#endif
 	}
-	
-	
+
 	msg[thread_id].type = -1;
-	printf("THread %d is exiting \n", thread_id);
+	printf("Thread %d is exiting \n", thread_id);
 	pthread_mutex_unlock(&mutex_var[thread_id]);
 	pthread_exit((void*) 0);
 }
@@ -79,29 +101,33 @@ void* ipc_controller(void *arg){
     while(1){
         int i =0;
         int deadlock = 0;
-        
+        int flag2=0;
         for(i=0;i<NUM_THREADS;i++){
             if(pthread_mutex_trylock(&mutex_var[i]) == 0){
-   //             printf("IPC :: Wait for thread %d\n",i);
-   //             printf("OP is %d \n" ,msg[i].type);
+				lockedMutex[i]=1;
+            #if(DEBUG)
+                printf("IPC :: Wait for thread %d\n",i);
+                printf("OP is %d \n" ,msg[i].type);
                 // updated struct is here now, read it.
+            #endif
                 if(msg[i].type == SEND){ 
                     if(bufferFull ==0){
                       messages[empty_block] = msg[i]; 
                       empty_block = (empty_block+1)%MAX_BUFFER_SIZE;
                       if(empty_block == start_block){
-     //                       printf("Buffer full is happening here \n");
                             bufferFull = 1;
                       }
-                    msg[i].type =-1;
-     //               printf("Process %d wants to send a message\n ",i); 
+                    printf("Message Sent: %d, %d, %s\n ", i , msg[i].receiver , msg[i].msg);
                     pthread_cond_signal(&sem_var[i]); //mutex is temporarily locked at this point
+                    lockedMutex[i]=0;
+                    msg[i].type =-1;
                     pthread_mutex_unlock(&mutex_var[i]);
-   //                 printf("Process %d sent a message\n ",i); 
                     }
                     else{
 	//					printf("Process %d wants to send a message but the buffer is full! Sorry\n ",i); 
                         deadlock++;
+                        lockedMutex[i]=0;
+                        pthread_mutex_unlock(&mutex_var[i]);
                         continue; //dont signal the process here let it wait for the buffer to get empty
                     }
                 }
@@ -113,7 +139,7 @@ void* ipc_controller(void *arg){
                             strcpy(msg[i].msg ,  messages[j].msg);
                             msg[i].sender = messages[j].sender;
                             //msg[i].type = RECEIVE;
-      //                      printf("Process %d wants to recieve a message, message to receive %s\n ",i,messages[j].msg); 
+                           printf("Message Received: %d, %d, %s\n ",messages[j].sender , i,messages[j].msg); 
                             if(j ==start_block){
                                 start_block = (start_block +1) %MAX_BUFFER_SIZE;
                                 if(start_block !=empty_block){
@@ -132,27 +158,59 @@ void* ipc_controller(void *arg){
                             }
                             flag=1;
                             pthread_cond_signal(&sem_var[i]); //mutex is temporarily locked at this point
+                            lockedMutex[i]=0;
                             pthread_mutex_unlock(&mutex_var[i]);
                             break;
                         }
                    }
+                   
                    if(flag==0){
-					pthread_mutex_unlock(&mutex_var[i]);
 					deadlock++;
+					lockedMutex[i]=0;
+				//	pthread_cond_signal(&sem_var[i]); //mutex is temporarily locked at this point  
+					pthread_mutex_unlock(&mutex_var[i]);
 					}
                 }
-                else{
-					//printf("No request from thread %d \n",i);
-					deadlock++;
-					pthread_mutex_unlock(&mutex_var[i]);
+                
+                else if(msg[i].type ==-1){
+			#if(DEBUG)
+					printf("No request from thread %d \n",i);
+			#endif
+				//	deadlock++;
 					continue;
 				}
+				
+			   else{
+				    flag2++;
+				    deadlock++;
+//					printf("%d \n" , msg[i].type);
+					lockedMutex[i]=0;
+					pthread_mutex_unlock(&mutex_var[i]);
+				}	
             }
         }
         
         //printf("deadlock is %d\n",deadlock);
-        if(deadlock==NUM_THREADS)
+        if(deadlock==NUM_THREADS){
+			continue;
+			printf("Exiting IPC Controller \n");
+			deadlock_detected = 1;
+			int h;
+			int flag1;
+			for(h=0;h<NUM_THREADS;h++){
+				if(lockedMutex[h]==1){
+  					  flag1 =1;
+				      pthread_cond_signal(&sem_var[i]); //mutex is temporarily locked at this point
+                      pthread_mutex_unlock(&mutex_var[i]);
+				}
+			}
+			if(flag2 != 4)
+				printf("Deadlock Detected\n");
+			else
+				printf("Completed Message Passing No more messages\n");
 			pthread_exit (NULL);	
+		}
+		
     }
 } 
 
@@ -162,7 +220,8 @@ int main(){
 	pthread_attr_t attr;
 	int i;
 	  /* Initialize mutex and condition variable objects */
-	for(i=0;i<NUM_THREADS;i++){ 
+	for(i=0;i<NUM_THREADS;i++){
+			  lockedMutex[i]=0;
 			  pthread_mutex_init(&mutex_var[i], NULL);
 			  pthread_cond_init (&sem_var[i], NULL);
 	}
@@ -178,20 +237,19 @@ int main(){
 	pthread_create(&threads[i], &attr, ipc_controller , (void *)i);
 	
 	 /* Wait for all threads to complete */
+	
 	for (i = 0; i < NUM_THREADS; i++) {
 		pthread_join(threads[i], NULL);
+		msg[i].type = -2;
 	}
+	
 	
 	
 	pthread_attr_destroy(&attr);
-	for(i=0; i<NUM_THREADS;i++){
-		pthread_mutex_destroy(&mutex_var[i]);
-		pthread_cond_destroy(&sem_var[i]);
-	}
+//	destroy_mutex();
 	
-	printf("Exiting main Thread :\n");
+	printf("Exiting main Thread \n");
 	
-//	pthread_cancel(threads[NUM_THREADS]);
 	pthread_exit (NULL);	
 	
 	
