ae699ca72d59852c9311c0223ce2f66535d3befe
Nilesh Kulkarni <nileshsatishkulkarni@gmail.com>
2014-03-07 18:49:39 +0530

    test files added

--- VirtualDisk/guestos/1.c
+++ VirtualDisk/guestos/1.c
@@ -8,12 +8,14 @@ int main(){
     int c = (int)a;
    // printf("Typecasted check : %p %d %p \n" , a , c , (void *)c); 
     
-    //printf("About to make syscall's  %d\n", (int) a);
-      
-    syscall(351,0,sizeof(int)*100,(int)a,1,1);
-    //printf("Done with syscall 1\n");
 	syscall(351,1,sizeof(int)*100,(int)b,1,1);
-     printf("Done with syscall  write2\n");
+    //printf("About to make syscall's  %d\n", (int) a);
+    while(1){
+        printf("kakuji roxx!! \n");
+        syscall(351,0,sizeof(int)*100,(int)a,1,1);
+//        printf("Done with syscall  write2\n");
+    }
+    //printf("Done with syscall 1\n");
 //	printf(((*b == 10)? "Write works, b is %d \n" : "Write doesn't work , b is %d \n"),*b);
 	return 1;
 }
--- VirtualDisk/guestos/2.c
+++ VirtualDisk/guestos/2.c
@@ -1,19 +1,10 @@
 #include<stdio.h>
 
 int main(){
-    
-    int *a = malloc(sizeof(int)*100);
-    *a = 10;
-    int *b = malloc(sizeof(int)*100); 
-    int c = (int)a;
-   // printf("Typecasted check : %p %d %p \n" , a , c , (void *)c); 
-    
-    //printf("About to make syscall's  %d\n", (int) a);
-      
-    syscall(351,0,sizeof(int)*100,(int)a,10,10);
-    //printf("Done with syscall 1\n");
-	syscall(351,1,sizeof(int)*100,(int)b,10,10);
-     printf("Done with syscall  write2\n");
-//	printf(((*b == 10)? "Write works, b is %d \n" : "Write doesn't work , b is %d \n"),*b);
-	return 1;
+    int a =0; 
+	while(1){
+    a++;
+    printf("Yes!!\n");
+    }
+    return 1;
 }
